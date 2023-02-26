5af63bcd5044b21609437ce1f803e387f1d48e73
Zoe Anne Dyson <zadyson@gmail.com>
2023-02-26 18:57:14 +0000

    Update getReads.sh

--- getReads.sh
+++ getReads.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # Downloads multiple read sets from ena using entrez
-# using a file with one data accession per line
+# using a file with one data accession per line. Won't 
+# overwrite data if some accessions/files fail.
 # Zoe Dyson zoe.dyson(at)lshtm.ac.uk
 # 24/02/2023
 # Example usage 'bash getReads.sh > getReadsLog.txt'
