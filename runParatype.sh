26b6c6e2bfde09240cb58f49f8e3e471256a985e
Zoe Anne Dyson <zadyson@gmail.com>
2023-02-27 16:55:09 +0000

    Create runParatype.sh

---
+++ runParatype.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Run Paratype on multiple files.
+#
+# Zoe Dyson zoe.dyson(at)lshtm.ac.uk
+# 27/02/2023
+# Example usage 'bash runParatype  > runParatypeLog.txt'
+#
+# Loop through accessions in file
+while read file;
+ # run paratype on each accession
+ echo python3 ~/software/Paratype/paratype.py --id $file --mode fastq --fastq ../reads/${file}/${file}_1.fastq.gz ../reads/${file}/${file}_2.fastq.gz --output ${file}_paratype.txt
+# get accesion numbers from file 'ids.txt'
+done<ids.txt
