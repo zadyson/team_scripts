337d6695dc7d947d8ed7672133775e6d1b735b38
Zoe Anne Dyson <zadyson@gmail.com>
2023-04-02 14:50:38 +0100

    Create runAribaPlasmidFinderMassive.sh

---
+++ runAribaPlasmidFinderMassive.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --qos=normal
+#SBATCH --account=js66
+#SBATCH --job-name="pf_ariba"
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=24576
+#SBATCH --time=119:00:00
+
+module load ariba/2.14.4
+
+# Loop through accessions in file
+while read file;
+ # run ariba on each read set with the card database
+ do ariba run plasmidfinder.out ../reads/$file/${file}_1.fastq.gz ../reads/$file/${file}_2.fastq.gz ${file} --threads 1
+# get accessions from file 'ids.txt'
+done<../ids.txt
