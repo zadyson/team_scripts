888677b4fb171707720e41f2fe8f36b068c961c0
Zoe Anne Dyson <zadyson@gmail.com>
2023-10-04 12:26:32 +0100

    Update getReadsSanger.sh

--- getReadsSanger.sh
+++ getReadsSanger.sh
@@ -9,12 +9,11 @@
 # Load modules
 module load ena-browser-tools/1.6
 module load bsub.py/0.42.1
-
 # Loop through accessions in file
 while read file;
  # fetch data by accession with enterz and
  # save in fastq format in folder 'reads'
- do bsub.py 1 $file  enaDataGet -f fastq -d reads $file;
+ do bsub.py 1 $file  /data/pam/typhinet/scratch/zd2/2023_TyphiNET_ParatyphiA/software/enaBrowserTools/python3/enaDataGet -f fastq -d reads $file;
  sleep 1
 # get accesion numbers from file 'ids.txt'
 done<ids.txt
