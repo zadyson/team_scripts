988c6523ecd52b0932ad065c9d40e8ecc399d9b7
Zoe Anne Dyson <zadyson@gmail.com>
2023-10-04 16:50:38 +0100

    Create runParatypeSanger.sh

---
+++ runParatypeSanger.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Run Paratype on multiple files.
+#
+# Zoe Dyson zoe.dyson(at)lshtm.ac.uk
+# Updated for Sanger cluster 04/10/23
+# Example usage 'bash runParatype  > runParatypeLog.txt'
+#
+# Load modules
+module purge
+module load bsub.py
+module load bowtie2/2.3.5--py37he860b03_0
+module load bcftools/1.9--h68d8f2e_9
+module load samtools/1.9--h91753b0_8
+
+# Activate python3 env with bio
+source my_project/bin/activate
+
+# Loop through accessions in file
+while read file;
+ # run paratype on each accession
+ #do bsub.py 16 $file python3 /data/pam/typhinet/scratch/zd2/2023_TyphiNET_ParatyphiA/software/Paratype/paratype.py --id $file --mode fastq --fastq ../reads/${file}/${file}_1.fastq.gz ../reads/${file}/${file}_2.fastq.gz --threads 8 --output ${file}_paratype.txt
+ do bsub -E 'test -e /nfs/users/nfs_z/zd2' -R "select[mem>16000] rusage[mem=16000]" -M16000 -o ERR10293809.o -e ERR10293809.e -J ERR10293809 -n8 python3 /data/pam/typhinet/scratch/zd2/2023_TyphiNET_ParatyphiA/software/Paratype/paratype.py --id $file --mode fastq --fastq ../reads/${file}/${file}_1.fastq.gz ../reads/${file}/${file}_2.fastq.gz --threads 8 --output ${file}_paratype.txt
+# get accesion numbers from file 'ids.txt'
+done<../ids.txt
